f429edb0be9a91640d8a4dbf4cf10969750aa874
admin <whinedine8@gmail.com>
2022-03-28 13:45:15 +0300

    working on tiles

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from tkinter import *
 
 class App:
     def __init__(self,w,h):
+
+        #consts
+        self.SIZE = 40
+
         #root settings
         self.h,self.w = h,w
         self.root = Tk()
@@ -15,7 +19,26 @@ class App:
         self.cv = Canvas(self.root,width=self.w,height=self.w,bg='black')
         self.cv.pack()
 
+
+        self.grid = [[0] * 8 for _ in range(8)]
+        self.tiles = [[] for _ in range(8)]
+
+        for i in range(0,self.SIZE*8,self.SIZE):
+            for j in range(0,self.SIZE*8,self.SIZE):
+                tile = self.cv.crea
+                self.tiles[i].append()
+
         
+
+
+    def drawMaze(self):
+        #needs editing
+        for i in range(0,self.SIZE*8,self.SIZE):
+            for j in range(0,self.SIZE*8,self.SIZE):
+                tile = self.cv.crea
+                self.tiles[i].append()
+
+
 
 
 
